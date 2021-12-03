dea4bbb56941271186550c0b0a6c0e0da10dad6d
Siyuan PENG <sn0wbar@qq.com>
2021-12-03 09:28:42 -0500

    Updated corrected files

--- gridsearch3.py
+++ gridsearch3.py
@@ -9,7 +9,7 @@ ezreader = EZReader(corpus="datasets/8_1_corpus.txt",
                     target="datasets/8_1_target.txt",
                     simulation="worddvs",
                     num_subject=29,
-                    output='outputs/result3.txt')
+                    output='outputs/result5.txt')
 
 a1 = np.arange(50, 280, 3)
 a2 = np.arange(1, 5, 0.5)
@@ -21,9 +21,9 @@ pf = np.arange(0.01, 0.12, 0.04)
 
 params = product(a1, a2, a3, d, i, pf)
 
-params = list(params)[80000:90000]
+params = list(params)[170000:180000]
 
-index = 80000
+index = 170000
 results = pd.DataFrame(
     columns=['index', 'SFD', 'FFD', 'GD', 'TT', 'PrF', 'Pr1', 'Pr2', 'PrS', 'token'])
 
@@ -36,4 +36,4 @@ for param in tqdm(params):
     results = results.append(result_per_param_set, ignore_index=True)
     index += 1
 
-results.to_csv('EZReader_R8S1_800_900.csv')
+results.to_csv('EZReader_R8S1_1700_1800.csv')
